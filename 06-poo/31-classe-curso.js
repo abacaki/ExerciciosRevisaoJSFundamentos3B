d266dce18ec0f6ce53cb1cc1c4b0ca1e540deca7
abacaki <abacaki98@gmail.com>
2026-08-25 16:46:27 -0300

    Poo ex. 31

--- 06-poo/31-classe-curso.js
+++ 06-poo/31-classe-curso.js
@@ -5,3 +5,19 @@
 //
 // Escreva sua solução abaixo:
 
+class Curso {
+    constructor(nome, duracao, modalidade) {
+        this.nome = nome;
+        this.duracao = duracao;
+        this.modalidade = modalidade
+    }
+    exibirInformacoes(){
+      console.log(`Curso: ${this.modalidade}; Duração: ${this.duracao}`);
+    }
+}
+
+
+const cursos = new Curso("DS", "2 anos", "Back-End");
+cursos.exibirInformacoes();
+const cursos1 = new Curso("ADM", "2 anos", "PPP");
+cursos1.exibirInformacoes();
