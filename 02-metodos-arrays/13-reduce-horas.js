0c37f55ac21a154f9ffeecd1ad712b8cb2ffa40b
abacaki <abacaki98@gmail.com>
2026-08-25 13:43:10 -0300

    Array ex. 12 e 13

--- 02-metodos-arrays/13-reduce-horas.js
+++ 02-metodos-arrays/13-reduce-horas.js
@@ -5,3 +5,7 @@
 //
 // Escreva sua solução abaixo:
 
+const horas = [8, 7, 8, 6, 8];
+
+const horasTrabalhadas = horas.reduce((acumulador, horas) => acumulador + horas);
+console.log(horasTrabalhadas);
